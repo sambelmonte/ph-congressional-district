7203581664f40177de942bf7cea960d07f61b1aa
Sam Belmonte <sam.belmonte@gmail.com>
2018-11-08 18:20:01 +0800

    Added script for mysql

---
+++ tables.sql
@@ -0,0 +1,8 @@
+create table Places(
+  PlaceID int not null,
+  Name varchar(255) not null,
+  Type varchar(255) not null,
+  Region varchar(255) not null,
+  Population int not null,
+  Primary Key(PlaceID)
+);
